94840f5e50c844adc9e368a7f0a23fd127fa78fc
xchenhao <chenhao_apple@163.com>
2022-05-17 22:25:22 +0800

    添加链表虚拟头节点优化版本

---
+++ README.md
@@ -0,0 +1,10 @@
+## 数据结构与算法
+
+- 斐波那契数列：`com.demo.Fibonacci`
+- 动态数组：`com.demo.ArrayList`
+- 单向链表：
+  + `com.demo.LinkedList`
+  + `com.demo.LinkedList2` 虚拟头节点优化版本
+  + LeetCode: `com.leetcode.linkedlist.Solution`
+    * 反转链表（递归、迭代）
+    * 判断链表是否有环
--- src/com/demo/LinkedListTest.java
+++ src/com/demo/LinkedListTest.java
@@ -16,6 +16,18 @@ public class LinkedListTest {
         list.remove(1);
 
         System.out.println(list);
+    }
 
+    @Test
+    public void test2() {
+        List<Integer> list = new LinkedList2<>();
+        list.add(20);
+        list.add(0, 10);
+        list.add(30);
+        list.add(list.size(), 40);
+
+        list.remove(1);
+
+        System.out.println(list);
     }
 }
