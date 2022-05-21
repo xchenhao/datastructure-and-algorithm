fdedbe2f9ee3673f24c4b9d4ed047c65787e91af
xchenhao <chenhao_apple@163.com>
2022-05-22 00:14:23 +0800

    代码重构

--- src/datastructure/tree/BinarySearchTree.java
+++ src/datastructure/tree/BinarySearchTree.java
@@ -1,127 +1,13 @@
 package datastructure.tree;
 
-import com.tools.printer.BinaryTreeInfo;
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 // https://github.com/
 
 @SuppressWarnings("unchecked")
-public class BinarySearchTree<E> implements BinaryTreeInfo {
-    private int size; // 元素的个数
-
-    private Node<E> root; // 根节点
-
+public class BinarySearchTree<E> extends BinaryTree<E> {
     private Comparator<E> comparator; // 比较器
-
-    @Override
-    public Object root() {
-        return root;
-    }
-
-    @Override
-    public Object left(Object node) {
-        return ((Node<E>)node).left;
-    }
-
-    @Override
-    public Object right(Object node) {
-        return ((Node<E>)node).right;
-    }
-
-    @Override
-    public Object string(Object node) {
-        return ((Node<E>)node).element;
-    }
-
-    private static class Node<E> {
-        // 存放元素
-        E element;
-        // 左节点
-        Node<E> left;
-        // 右节点
-        Node<E> right;
-        // 父节点
-        Node<E> parent;
-        // 构造函数
-        public Node(E element, Node<E> parent) {
-            this.element = element;
-            this.parent = parent;
-        }
-
-        public boolean isLeaf() {
-            return left == null && right == null;
-        }
-
-        public boolean hasTwoChildren() {
-            return left != null && right != null;
-        }
-    }
-
-    public void inOrder(Visitor<E> visitor) {
-        if (visitor == null) return;
-        inOrder(root, visitor);
-    }
-    private void inOrder(Node<E> node, Visitor<E> visitor) {
-        if (node == null || visitor.stop) return;
-        inOrder(node.left, visitor);
-        if (visitor.stop) return;
-        visitor.stop = visitor.visit(node.element);
-        inOrder(node.right, visitor);
-    }
-
-    public void postOrder(Visitor<E> visitor) {
-        if (visitor == null) return;
-        postOrder(root, visitor);
-    }
-    private void postOrder(Node<E> node, Visitor<E> visitor) {
-        if (node == null || visitor.stop) return;
-        postOrder(node.left, visitor);
-        postOrder(node.right, visitor);
-        if (visitor.stop) return;
-        visitor.stop = visitor.visit(node.element);
-    }
-
-    public void preOrder(Visitor<E> visitor) {
-        if (visitor == null) return;
-        preOrder(root, visitor);
-    }
-    private void preOrder(Node<E> node, Visitor<E> visitor) {
-        if (node == null || visitor.stop) return;
-        visitor.stop = visitor.visit(node.element);
-        preOrder(node.left, visitor);
-        preOrder(node.right, visitor);
-    }
-
-    public void levelOrder(Visitor<E> visitor) {
-        if (root == null || visitor == null) return;
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            Node<E> node = queue.poll();
-
-            if (visitor.visit(node.element)) {
-                break;
-            }
-
-            if (node.left != null) {
-                queue.offer(node.left);
-            }
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
-        }
-    }
-
-    /**
-     * @param <E>
-     * @return 如果返回 true，就代表停止遍历
-     */
-    public static abstract class Visitor<E> {
-        boolean stop;
-        abstract boolean visit(E element);
-    }
 
     private void elementNotNullCheck(E element) {
         if (element == null) {
@@ -148,19 +34,6 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         return ((java.lang.Comparable<E>)e1).compareTo(e2);
     }
 
-    // 元素的数量
-    public int size() {
-        return size;
-    }
-    // 是否为空
-    public boolean isEmpty() {
-        return size == 0;
-    }
-    // 清空所有元素
-    public void clear() {
-        root = null;
-        size = 0;
-    }
     // 添加元素
     public void add(E element) {
         // 判断节点的值是否为空。
@@ -402,102 +275,4 @@ public class BinarySearchTree<E> implements BinaryTreeInfo {
         toString(node.right, sb, prefix + "R---");
     }
 
-    // 通过递归实现获取树的高度
-    public int height() {
-        return height(root);
-    }
-
-    private int height(Node<E> node) {
-        if (node == null) return 0;
-        return 1 + Math.max(height(node.left), height(node.right));
-    }
-
-    // 通过迭代（层序遍历）实现获取树的高度
-    public int height2() {
-        if (root == null) return 0;
-        int height = 0;  // 树的高度
-        int levelSize = 1;  // 存储着每一层的高度
-
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-        while (!queue.isEmpty()) {
-            Node<E> node = queue.poll();
-            levelSize--;
-
-            if (node.left != null) {
-                queue.offer(node.left);
-            }
-            if (node.right != null) {
-                queue.offer(node.right);
-            }
-
-            if (levelSize == 0) {  // 意味着即将要访问下一层
-                levelSize = queue.size();
-                height++;
-            }
-        }
-
-        return height;
-    }
-
-    // 判断是否是一棵完全二叉树
-    public boolean isComplete() {
-        if (root == null) return false;
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-
-        boolean leaf = false;
-        while (!queue.isEmpty()) {
-            Node<E> node = queue.poll();
-            if (leaf && !node.isLeaf()) {
-                return false;
-            }
-
-            if (node.hasTwoChildren()) {
-                queue.offer(node.left);
-                queue.offer(node.right);
-            } else if (node.left == null && node.right != null) {
-                return false;
-            } else {  // 后面遍历的节点都必须是叶子节点
-                leaf = true;
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-            }
-        }
-
-        return true;
-    }
-
-    public boolean isComplete2() {
-        if (root == null) return false;
-        Queue<Node<E>> queue = new LinkedList<>();
-        queue.offer(root);
-
-        boolean leaf = false;
-        while (!queue.isEmpty()) {
-            Node<E> node = queue.poll();
-
-            if (leaf && !node.isLeaf()) {
-                return false;
-            }
-
-            if (node.left != null) {
-                queue.offer(node.left);
-            } else if (node.right != null) {
-                // node.left == null && node.right != null
-                return false;
-            }
-
-            if (node.right != null) {
-                queue.offer(node.right);
-            } else {
-                // node.left == null && node.right == null
-                // node.left != null && node.right == null
-                leaf = true;
-            }
-        }
-
-        return true;
-    }
 }
